5d9dc3fed26a993eefb6a7691414ab84ed307bf3
taniTk <tanitk91@gmail.com>
2017-01-01 19:29:18 +0900

    非同期処理にしてみた

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@
 
 import urllib.request, urllib.parse
 import json
+import asyncio
+import aiohttp
 
 
 end_point = "http://54.92.123.84/search?"
@@ -18,10 +20,23 @@ def generate_url(keyword):
     return end_point + urllib.parse.urlencode(params)
 
 
-def get_response(url):
-    with urllib.request.urlopen(url) as res:
-        json_res = json.loads(res.read().decode("utf-8"))
-        return json_res
+async def get_response(keyword, url):
+    # with urllib.request.urlopen(url) as res:
+    #     json_res = await json.loads(res.read().decode("utf-8"))
+    #     return json_res
+    response = await aiohttp.get(url)
+    data = await response.text()
+    json_data = json.loads(data)
+    return responce2simple_json(keyword, json_data)
+
+
+def responce2simple_json(keyword, res):
+    return {"name": keyword, "count": int(res["response"]["result"]["numFound"])}
+
+
+def calc_max_numFound(results):
+    for result in results:
+        print(result)
 
 
 def print_for_test(ans):
@@ -29,13 +44,17 @@ def print_for_test(ans):
 
 
 def main(argv):
-    ans_lst = []
+    urls = []
+
     for keyword in argv:
         url = generate_url(keyword)
+        urls.append({'url': url, 'keyword': keyword})
         # print(url)
-        res = get_response(url)
-        # print(res)
-        ans = {"name":keyword, "count":int(res["response"]["result"]["numFound"])}
-        ans_lst.append(ans)
+        # res = asyncio.ensure_future(get_response(url))
+        # ans = {"name":keyword, "count":int(res["response"]["result"]["numFound"])}
+        # ans_lst.append(ans)
     # print(ans_lst)
-    print_for_test(max(ans_lst, key=lambda ans: int(ans["count"])))
+    futures = [get_response(url['keyword'], url['url']) for url in urls]
+    loop = asyncio.get_event_loop()
+    tasks = loop.run_until_complete(asyncio.wait(futures))[0]
+    print_for_test(max(tasks, key=lambda task: int(task.result()["count"])).result())
