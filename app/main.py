7856e026fa6b6e587d5e39432565ecf60cee6eb6
taniTk <tanitk91@gmail.com>
2017-01-01 20:13:26 +0900

    リファクタリング，コメント追加

--- app/main.py
+++ app/main.py
@@ -16,11 +16,17 @@ params = {
 
 
 def generate_url(keyword):
+    '''
+    keywordを含む記事を検索するAPIを生成
+    '''
     params["q"] = "Body:" + str(keyword)
     return end_point + urllib.parse.urlencode(params)
 
 
 async def get_response(keyword, url):
+    '''
+    keywordとそれに対応するAPIのURLを叩いて結果をいい感じのJSONにする
+    '''
     response = await aiohttp.get(url)
     data = await response.text()
     json_data = json.loads(data)
@@ -28,15 +34,23 @@ async def get_response(keyword, url):
 
 
 def responce2simple_json(keyword, res):
+    '''
+    返ってきたJSONから必要な部分だけを抽出
+    '''
     return {"name": keyword, "count": int(res["response"]["result"]["numFound"])}
 
 
-def calc_max_numFound(results):
-    for result in results:
-        print(result)
+def calc_max_numFound(tasks):
+    '''
+    すべての結果(Taskのset)からnumFoundが最大のものをdictとして返す
+    '''
+    return max(tasks, key=lambda task: int(task.result()["count"])).result()
 
 
 def print_for_test(ans):
+    '''
+    ans(Dict型)をテストで求められる形式に変換
+    '''
     print("{\"name\":\"" + ans["name"] + "\",\"count\":" + str(ans["count"]) + "}")
 
 
@@ -50,4 +64,4 @@ def main(argv):
     futures = [get_response(url['keyword'], url['url']) for url in urls]
     loop = asyncio.get_event_loop()
     tasks = loop.run_until_complete(asyncio.wait(futures))[0]
-    print_for_test(max(tasks, key=lambda task: int(task.result()["count"])).result())
+    print_for_test(calc_max_numFound(tasks))
